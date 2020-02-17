90dd8daaa6ada99a262bab8c0299fc470e5a3058
Lidiane Telles <lidiane.telles@elotech.com.br>
2020-02-17 17:24:41 -0300

    alterando json

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -44,7 +44,7 @@ module.exports = {
                 browserName: "chrome",
                 chromeOptions: {
                     w3c: false,
-                    args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage']
+                    args: ['--headless', '--no-sandbox']
                 }
             }
         },
